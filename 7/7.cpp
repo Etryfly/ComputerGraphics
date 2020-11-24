25d7927a34832d41a848944d112c1f3008f70cfd
etrymax@gmail.com <etrymax@gmail.com>
2020-11-24 17:29:20 +0300

    Done

--- 7/7.cpp
+++ 7/7.cpp
@@ -3,18 +3,16 @@
 #include <GL/glut.h>  
 
 
- /* Global variables */
-char title[] = "3D Shapes";
+char title[] = "7";
 float angle =0;
 
-/* Initialize OpenGL Graphics */
 void initGL() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-    glClearDepth(1.0f);                   // Set background depth to farthest
-    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-    glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-    glShadeModel(GL_SMOOTH);   // Enable smooth shading
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearDepth(1.0f);                 
+    glEnable(GL_DEPTH_TEST);  
+    glDepthFunc(GL_LEQUAL);    
+    glShadeModel(GL_SMOOTH);  
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
 }
 
 void timer(int arg)
@@ -25,31 +23,25 @@ void timer(int arg)
 }
 
 
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);   
     glOrtho(-2.0, 2.0, -2.0, 2.0, -1.5, 1.5);
     
-    // Render a color-cube consisting of 6 quads with different colors
-  
-    // Render a pyramid consists of 4 triangles
-    glLoadIdentity();                  // Reset the model-view matrix
-  //  glTranslatef(-2, 0.0f, -5.0f);  // Move left and into the screen
+   
+    glLoadIdentity();                  
     glTranslatef(1.5f, 0.0f, -7.0f);
     glRotatef(angle, 0.0, 1.0, 0.0);
     glRotatef(angle, 1.0, 1.0, 0.0);
-   // glRotatef(angle, 1.0, .0, 0.0);
 
-    glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
+    glBegin(GL_TRIANGLES);          
       
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glColor3f(1.0f, 0.0f, 0.0f);   
     glVertex3f(-1, 2, 0);
     glVertex3f(2, 0, 0);
     glVertex3f(-1, -2, 0);
   
-    glColor3f(1.0f, 1.0f, 1.0f);     // Red
+    glColor3f(1.0f, 1.0f, 1.0f);    
     glVertex3f(0, 1,1);
     glVertex3f(1, 0, 1);
     glVertex3f(0, -1, 1);
@@ -82,44 +74,36 @@ void display() {
 
    
 
-    glLoadIdentity();                  // Reset the model-view matrix
+    glLoadIdentity();                  
     glTranslatef(-1.5f, 0.0f, -6.0f);
 
-    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+    glutSwapBuffers(); 
     glutTimerFunc(1, timer, 0);
 }
 
 
-
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-   // Compute aspect ratio of the new window
-    if (height == 0) height = 1;                // To prevent divide by 0
+void reshape(GLsizei width, GLsizei height) {  
+    if (height == 0) height = 1;                
     GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-    // Set the viewport to cover the new window
     glViewport(0, 0, width, height);
 
-    // Set the aspect ratio of the clipping volume to match the viewport
-    glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-    glLoadIdentity();             // Reset
-    // Enable perspective projection with fovy, aspect, zNear and zFar
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity();            
     gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
 
-/* Main function: GLUT runs as a console application starting at main() */
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);            // Initialize GLUT
-    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-    glutInitWindowSize(640, 480);   // Set the window's initial width & height
-    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-    glutCreateWindow(title);          // Create window with the given title
-    glutDisplayFunc(display);       // Register callback handler for window re-paint event
-    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-    initGL();                       // Our own OpenGL initialization
+    glutInit(&argc, argv);          
+    glutInitDisplayMode(GLUT_DOUBLE); 
+    glutInitWindowSize(640, 480);  
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow(title);          
+    glutDisplayFunc(display);      
+    glutReshapeFunc(reshape);      
+    initGL();                       
 
     
-    glutMainLoop();                 // Enter the infinite event-processing loop
+    glutMainLoop();                
     return 0;
 }
